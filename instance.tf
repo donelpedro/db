c68b0dfd91473277a1f6b4956f173f971c3a1720
pitpawa <petro.maliyenko@pawatech.com>
2024-06-18 11:50:06 +0200

    init

---
+++ instance.tf
@@ -0,0 +1,25 @@
+resource "google_compute_instance" "default" {
+  name         = "terraform-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  tags = ["web", "dev"]
+}
+
